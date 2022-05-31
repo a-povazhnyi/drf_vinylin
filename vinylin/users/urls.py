313bf8d4d59d27b9a492852fe2fc033464b19308
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-31 16:09:06 +0300

    feat: users urls init

---
+++ vinylin/users/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from users.views import (
+    UserViewSet,
+    EmailVerificationView,
+    EmailConfirmView,
+    EmailChangeView,
+    PasswordChangeView,
+)
+
+
+router = SimpleRouter()
+router.register(r'', UserViewSet)
+
+urlpatterns = [
+    path('email/verification/', EmailVerificationView.as_view()),
+    path('email/confirm/<str:token>/', EmailConfirmView.as_view()),
+    path('email/change/', EmailChangeView.as_view()),
+
+    path('password/change/', PasswordChangeView.as_view()),
+]
+urlpatterns += router.urls
