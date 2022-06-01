c931e0f34f047d7cb0c11393318cae8cdb04f181
a-povazhnyi <a.povazhnyi@wis.software>
2022-06-01 12:38:15 +0300

    fix: del extra url param

--- vinylin/users/urls.py
+++ vinylin/users/urls.py
@@ -15,7 +15,7 @@ router.register(r'', UserViewSet)
 
 urlpatterns = [
     path('email/verification/', EmailVerificationView.as_view()),
-    path('email/confirm/<str:token>/', EmailConfirmView.as_view()),
+    path('email/confirm/', EmailConfirmView.as_view()),
     path('email/change/', EmailChangeView.as_view()),
 
     path('password/change/', PasswordChangeView.as_view()),
