c65592580d97767e9b61c1fc70e2fabd51124965
a-povazhnyi <a.povazhnyi@wis.software>
2022-06-02 16:00:25 +0300

    fix: password_validation moved to validators.py

--- vinylin/users/validators.py
+++ vinylin/users/validators.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
+from django.contrib.auth import password_validation
+from django.core import exceptions
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
 
 
 def birthday_validator(birthday):
@@ -15,3 +18,17 @@ def birthday_validator(birthday):
         raise ValidationError(
             _('Your birthday couldn\'t have been that long ago')
         )
+
+
+def _validate_password(password, user):
+    errors = {}
+    try:
+        password_validation.validate_password(
+            password=password,
+            user=user
+        )
+    except exceptions.ValidationError as e:
+        errors['errors'] = list(e.messages)
+
+    if errors:
+        raise serializers.ValidationError(errors)
