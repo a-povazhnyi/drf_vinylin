a7c77292e4c6f559e0b041b66b8c8e0677d65e00
a-povazhnyi <a.povazhnyi@wis.software>
2022-06-02 16:04:00 +0300

    feat: UserViewSet router register

--- vinylin/users/urls.py
+++ vinylin/users/urls.py
@@ -1,27 +1,9 @@
-from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from users.views import (
-    UserViewSet,
-    EmailVerificationView,
-    EmailConfirmView,
-    EmailChangeView,
-    PasswordChangeView,
-    PasswordResetView,
-    PasswordResetConfirmView
-)
+from users.views import UserViewSet
 
 
 router = SimpleRouter()
-router.register(r'', UserViewSet)
+router.register('', UserViewSet)
 
-urlpatterns = [
-    path('email/verification/', EmailVerificationView.as_view()),
-    path('email/confirm/', EmailConfirmView.as_view()),
-    path('email/change/', EmailChangeView.as_view()),
-
-    path('password/change/', PasswordChangeView.as_view()),
-    path('password/reset/token/', PasswordResetView.as_view()),
-    path('password/reset/confirm/', PasswordResetConfirmView.as_view())
-]
-urlpatterns += router.urls
+urlpatterns = router.urls
