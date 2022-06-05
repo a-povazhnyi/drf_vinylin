c78e71d40e18464955a4e1980a0778339ef98482
a-povazhnyi <a.povazhnyi@wis.software>
2022-06-06 02:41:28 +0300

    feat: swagger

--- vinylin/vinylin/urls.py
+++ vinylin/vinylin/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
 urlpatterns = [
@@ -13,13 +16,44 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
     path('api/auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/',
+         TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/',
+         TokenRefreshView.as_view(),
+         name='token_refresh'),
 
     path('api/users/', include('users.urls')),
     path('api/vinyl/', include('vinyl.urls')),
     path('api/orders/', include('orders.urls')),
 ]
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Vinylin API",
+      default_version='v1',
+      description="This is OpenAPI for Vinylin Store",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+swagger_urlpatterns = [
+    path(r'^swagger(?P\.json|\.yaml)$',
+         schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
+    path('swagger/',
+         schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/',
+         schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc')
+]
+
+urlpatterns += swagger_urlpatterns
 
 if settings.DEBUG:
     import debug_toolbar
